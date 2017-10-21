5494b9d2da792a23b4fb94eff602b11474c1e7ab
“Sam <sam.alsmadi@gmail.com>
2017-10-21 16:42:09 -0400

    Add login routes

--- app.js
+++ app.js
@@ -167,6 +167,18 @@ app.post("/register", function(req, res){
     });
 });
 
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local",
+{
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+
+}),function(req, res){  
+});
+
 app.listen(3000, "localhost", function(){
     console.log("YelpCamp Server Has Started!");
 });
