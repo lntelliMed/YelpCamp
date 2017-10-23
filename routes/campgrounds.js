2bffa51ffbb029b38dacad20db2985d28e20e533
“Sam <sam.alsmadi@gmail.com>
2017-10-23 01:15:14 -0400

    Check if user is authorized to edit a campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -57,14 +57,11 @@ router.get("/:id", function(req, res){
     });
 });
 
-router.get("/:id/edit", function(req, res){
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res){
     Campground.findById(req.params.id, function(err, foundCampground){
-        if(err){
-            res.redirect("/campgrounds");
-        } else {
-            res.render("campgrounds/edit", {campground: foundCampground});            
-        }
+        res.render("campgrounds/edit", {campground: foundCampground});                                
     });
+
 });
 
 router.put("/:id", function(req, res){
@@ -94,4 +91,23 @@ function isLoggedIn(req, res, next){
     res.redirect("/login");
 }
 
+function checkCampgroundOwnership(req, res, next){
+    if(req.isAuthenticated()){
+        Campground.findById(req.params.id, function(err, foundCampground){
+            if(err){
+                res.redirect("back");
+            } else {
+                if(foundCampground.author.id.equals(req.user._id)){
+                    next();
+                } else {
+                    // res.send("Youd don't have the permission to do that!");
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        // console.log("You need to be logged in to do that!");
+        res.redirect("back");
+    }
+}
 module.exports = router;
