2c891541a37d014e52c74123cf336515fa96cf7b
“Sam <sam.alsmadi@gmail.com>
2017-10-23 01:39:09 -0400

    Check if user is authorized to delete  a campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -64,7 +64,7 @@ router.get("/:id/edit", checkCampgroundOwnership, function(req, res){
 
 });
 
-router.put("/:id", function(req, res){
+router.put("/:id", checkCampgroundOwnership, function(req, res){
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
         if(err){
             res.redirect("/campgrounds");
@@ -74,7 +74,7 @@ router.put("/:id", function(req, res){
     });
 });
 
-router.delete("/:id", function(req, res){
+router.delete("/:id", checkCampgroundOwnership, function(req, res){
     Campground.findByIdAndRemove(req.params.id, req.body.campground, function(err, updatedCampground){
         if(err){
             res.redirect("/campgrounds");
